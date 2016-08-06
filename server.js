1a7ffe7f44da7b3572976c1635807c7005f967a7
Mary E. Ellis <mary.e.ellis@gmail.com>
2016-08-06 19:19:47 -0400

    Another tweak to see if db works

--- server.js
+++ server.js
@@ -5,8 +5,8 @@
     var app     = express();
     var mongoose  = require('mongoose');
     var port = process.env.PORT || 8080;
-    var database = require('./config/database');
-      var morgan    = require('morgan')
+    //var database = require('./config/database');
+    var morgan    = require('morgan')
     var bodyParser = require('body-parser');
     var methodOverride = require('method-override');
 
