068a907e1c14f53b3db7c50c8280d3d3ee3dfacf
Mary E. Ellis <mary.e.ellis@gmail.com>
2016-08-11 16:24:56 -0400

    Updated the server.js to include a specific port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
     var express = require('express');
     var app     = express();
     var mongoose  = require('mongoose');
-    var port = process.env.PORT;
+    var port = process.env.PORT || 8080;
     //var database = require('./config/database');
     var morgan    = require('morgan')
     var bodyParser = require('body-parser');
